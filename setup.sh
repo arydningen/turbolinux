bf2a0cbdab176783226690183f1a3c22ad047e00
arydningen <arydningen@gmail.com>
2023-07-18 00:35:35 +0200

    Some tweaks and changes

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 #cp -f ./bash/bash_alex ~/.bash_alex
 #cp -f ./zsh/zshrc ~/.zshrc
 
+# Create symlinks to the dotfiles
+ln -fs ~/.alex/bash/bash_aliases ~/.bash_aliases
 ln -fs ~/.alex/bash/bash_alex ~/.bash_alex
 ln -fs ~/.alex/zsh/zshrc ~/.zshrc
 
