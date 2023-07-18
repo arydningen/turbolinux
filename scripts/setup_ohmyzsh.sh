edcec7ab3ac5321304724c5e0f6227a584292161
arydningen <arydningen@gmail.com>
2023-07-18 19:30:33 +0200

    Added script for oh-my-zsh, and a theme (simple_alex)

---
+++ scripts/setup_ohmyzsh.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Just a quick script to run to install/refresh Oh-My-Zsh
+# I also does some other tricks so read through it
+
+# Remove any oh-my-zsh config
+rm -rf ~/.oh-my-zsh
+
+# Run the install script (unattended) for oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Download the zsh-autosuggestions plugin 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Modify .zshrc to set ZSH_THEME
+sed -i 's/ZSH_THEME.*/ZSH_THEME="simple_alex"/g' $HOME/.zshrc
+# Modify .zshrc to set zsh-autosuggestions
+sed -i 's/plugins=.*/plugins=(git zsh-autosuggestions)/g' $HOME/.zshrc
+
+# Add my aliases to .zshrc
+cat <<EOT >> ~/.zshrc
+if [ -f ~/.bash_aliases ]; then
+   . ~/.bash_aliases
+fi
+EOT
+
+# Symlink the simple_alex theme to the themes folder
+ln -fs ~/.alex/zsh/simple_alex.zsh-theme ~/.oh-my-zsh/themes/simple_alex-theme
+
---
+++ zsh/simple_alex.zsh-theme
@@ -0,0 +1,23 @@
+# Modified simple theme, the only change is the yellow gold color.
+
+# Colors for prompt
+idcolor="%B%F{#8700ff}"
+idcolor="%B%F{#875fd7}"
+dircolor="%B%F{#ffd700}"
+promptcolor="%B%F{#d70000}"
+
+
+
+# PROMPT='%(!.%{$fg[red]%}.%{$fg[green]%})%~$(git_prompt_info)%{$reset_color%} '
+PROMPT='${idcolor}%n@%m %(!.%{$fg[red]%}.%{$dircolor%})%~$(git_prompt_info)%{$reset_color%} '
+
+# If Im in tmux
+if { [ "$TMUX" ]; } then
+  # PS1="${idcolor}==> ${dircolor}%~${promptcolor}%#$reset_color "
+  PROMPT='%(!.%{$fg[red]%}.%{$dircolor%})%~$(git_prompt_info)%{$reset_color%} '
+fi
+
+ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg_bold[blue]%}("
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg_bold[blue]%})"
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[red]%}✗"
+ZSH_THEME_GIT_PROMPT_CLEAN=" %{$fg[green]%}✔"
