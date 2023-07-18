7676e1cdb7bf73a1cae012cce99eea4bb43590f8
arydningen <arydningen@gmail.com>
2023-07-18 03:22:06 +0200

    Annoying && nvim removed

--- scripts/setup_ohmytmux.sh
+++ scripts/setup_ohmytmux.sh
@@ -8,7 +8,7 @@ rm -rf ~/.config/tmux
 rm -rf ~/.local/share/tmux
 
 # Clone the repo directly to the .config folder
-git clone https://github.com/gpakosz/.tmux.git ~/.config/tmux --depth 1 && nvim
+git clone https://github.com/gpakosz/.tmux.git ~/.config/tmux --depth 1
 
 # Hack to just get it up and woriking
 mv $HOME/.config/tmux/.tmux.conf $HOME/.config/tmux/tmux.conf
